63256a41bb1357676b79e71cf9a6e5ffe810c946
Quadri Odunuga <quadriodunuga@Quadris-MacBook-Pro.local>
2023-04-05 12:29:21 +0100

    i edited the web page

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const nav_menu = document.getElementById('ham')
-const sidebar = document.getElementById('sidebar')
-nav_menu.addEventListener('click', function(){
-    sidebar.classList.toggle('d-none w-100')
-})
+// const nav_menu = document.getElementById('ham')
+// const sidebar = document.getElementById('sidebar')
+// nav_menu.addEventListener('click', function(){
+//     sidebar.classList.toggle('d-none w-100')
+// })
+
+ var modal= document.querySelector('.billpaymentmodal')  
+function openbillingmodal() {
+    modal.style.display='flex'
+}
+
+// var modal= document.querySelector('.paymentreceiptmodal')  
+// function openreceiptpaymentmodal() {
+//     modal.style.display='flex'
+// }
